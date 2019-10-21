733228c41562166d02bc4a9c640d4000f8ea481d
projetoclinica1 <56842285+projetoclinica1@users.noreply.github.com>
2019-10-21 15:03:56 -0300

    Add files via upload
    Esqueleto inicial do site.

---
+++ integrador1.0/index.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!--Css Externo-->
+    <link rel="stylesheet" href="css/estilos1.css">
+
+
+    <title>Clinica</title>
+  </head>
+  <body>
+<section class="container">  
+  <?php require_once "navbar.php"; ?>
+<!-- Slider-->
+<div class="bd-example ">
+  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="img/banner4.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="img/banner4.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Second slide label</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="img/banner4.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Third slide label</h5>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+<!--final Slide -->
+
+<!-- equipe-->
+<section class="orgSlide fundoF">
+  <div class="card-deck">
+    <div class="card" >
+      <img src="imgC/dr.png" class="card-img-top ajusteA" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Clinico Geral</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        
+      </div>
+    </div>
+    <div class="card">
+      <img src="imgC/diretor.png" class="card-img-top ajusteA" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Diretor da Rede</h5>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        
+      </div>
+    </div>
+    <div class="card">
+      <img src="imgC/recep.png" class="card-img-top ajusteA" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Recepcionista</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        
+      </div>
+    </div>
+  </div>
+</section>
+<!--final do time-->
+
+
+<!--planos-->
+
+<!--final Planos-->
+
+<!--Comentários-->
+
+<!-- footer-->
+<footer class="rodaP">
+	<div class="row">
+		<div class="col-xs-12">
+			<strong>Nome do Site</strong><br>
+			Criação Caseira<br>
+			Tel.:976977293 <i class="fa fa-phone fa-x1"></i> <br>
+			
+			E-mail:Nomedosite@gmail.com <i class="fa fa-envelope fa-x1"></i>
+		</div>
+	</div>
+</footer >
+<!--Final footer-->
+<section>
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ integrador1.0/navbar.php
@@ -0,0 +1,46 @@
+  
+  
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+  <a class="navbar-brand" href="#">Clinica inominada</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">inicio <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">sobre</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">"A decidir"</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Doutores</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Planos aceitos</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+         
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      
+    </ul>
+ 
+      
+      <button class="btn btn-danger my-2 my-sm-0" type="submit">Login</button>
+    
+  </div>
+</nav>
